d12bb5d3faf9d7de66c6a0d8291e00ee0b2b9c66
Oscar <akanegbuosinachi@gmail.com>
2026-08-11 00:21:05 +0100

    Add AddToCartButton component

---
+++ components/AddToCartButton.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useCart } from "./CartProvider";
+
+type AddToCartButtonProps = {
+  product: {
+    id: number;
+    name: string;
+    price: string;
+    image: string;
+  };
+};
+
+export default function AddToCartButton({
+  product,
+}: AddToCartButtonProps) {
+  const { addToCart } = useCart();
+
+  function handleAddToCart() {
+    addToCart(product);
+  }
+
+  return (
+    <button
+      onClick={handleAddToCart}
+      className="mt-4 w-full rounded-xl bg-black py-2.5 text-sm font-bold text-white transition hover:bg-[#D4AF37] hover:text-black"
+    >
+      Add to Cart
+    </button>
+  );
+}
